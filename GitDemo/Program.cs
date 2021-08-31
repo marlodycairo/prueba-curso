523c0de3aaa25533c965c65c74cd9b95e51d5652
Marlody <marlodycairo@hotmail.com>
2021-08-30 22:55:29 -0500

    Se crea el método para crear Person y se agrega una persona.

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GitDemo
 {
@@ -10,6 +11,12 @@ namespace GitDemo
 
             products.Nombre = "Atun en aceite.";
             products.Cantidad = 3;
+
+            Person person = new Person() { Nombre = "coco", Edad = "22", Ciudad = "Envigado", Email = "coco@gmail.com" };
+
+            var newperson = person.CrearPersona(person);
+
+            Console.WriteLine(newperson);
         }
     }
 
@@ -19,5 +26,19 @@ namespace GitDemo
         public string Edad { get; set; }
         public string Ciudad { get; set; }
         public string Email { get; set; }
+
+        public Person CrearPersona(Person person)
+        {
+            Person person1 = new Person()
+            {
+                Nombre = person.Nombre,
+                Edad = person.Edad,
+                Ciudad = person.Ciudad,
+                Email = person.Email
+            };
+            return person1;
+        }
     }
+
+    
 }
