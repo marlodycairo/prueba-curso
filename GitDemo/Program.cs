1a11f91840e3a6f9dcca25fda4d4558d78c22794
Marlody <marlodycairo@hotmail.com>
2021-08-30 20:20:11 -0500

    se agregó el archivo .gitignore

---
+++ GitDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Person
+    {
+        public string Nombre { get; set; }
+        public string Edad { get; set; }
+        public string Ciudad { get; set; }
+        public string Email { get; set; }
+    }
+}
